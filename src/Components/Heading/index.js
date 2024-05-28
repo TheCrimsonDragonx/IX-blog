a1ea362d20d606a4d80d807250e7300ee808a086
TheCrimsonDragonx <hadegoke.htcd@gmail.com>
2024-05-28 17:15:06 +0200

    may 28th

--- src/Components/Heading/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-export default function Heading() {
-  return <p className="page-title">THE 
-  BLOG</p>;
-}
--- src/Components/SubHeading/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import PropTypes from "prop-types";
-
-export default function SubHeading({ SubHeading }) {
-  return <p className="page-subtitle">{SubHeading}</p>;
-}
